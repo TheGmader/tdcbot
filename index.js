2619b9262e6650dee56aa81e492b1f92c7c92699
TheGmader <40711854+TheGmader@users.noreply.github.com>
2018-08-29 14:54:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ client.on('message', message => {
         if (message.content === prefix + "bbexamtheo"){
             var person = message.author.username
             client.users.get("281118889084059649").send(`<@`+ message.author.id +`> souhaite passer un examen théorique du brevet de base :wink:`)
-            client.users.get("226612789102247936").send(`<@`+ message.author.id +`> souhaite passer un examen théorique du brevet de base :wink:`)
             message.channel.send("Un message a été envoyé a un examinateur ! Il vous contactera au plus vite :wink:")
         }
         if (message.content === prefix + "pplexamtheo"){
